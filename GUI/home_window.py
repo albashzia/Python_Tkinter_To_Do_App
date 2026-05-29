fe32a057c87a16a143a66e6c047df0d569300efc
albashzia <ahmedalbashzia@gmail.com>
2026-05-29 16:27:33 +0500

    Creating a title_label and placing it on the window

--- GUI/home_window.py
+++ GUI/home_window.py
@@ -1,8 +1,17 @@
 from tkinter import *
+from tkinter.font import BOLD
 
 window = Tk()
 
 window.title("To Do App")
 window.geometry("350x500")
+
+title_label = Label(window,
+                    text="To-Do App",
+                    font=("Ink Free",20,BOLD),
+                    fg="black",
+                    bg="white"
+)
+title_label.place(x=105,y=10)
 
 window.mainloop()
